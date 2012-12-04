45a4fa0d58a546c1c05efa4bb978c1f6ebe49558
Pierre Barbier de Reuille <pierre.barbierdereuille@gmail.com>
2012-12-04 14:03:51 +0100

    Moved script to bin folder

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='PyQt-Fit',
       url=['https://code.google.com/p/pyqt-fit/'],
       packages= ['pyqt_fit', 'pyqt_fit/functions', 'pyqt_fit/residuals'],
       package_data = {'pyqt_fit': ['qt_fit.ui']},
-      scripts=['pyqt_fit1d.py'],
+      scripts=['bin/pyqt_fit1d.py'],
       requires=['setuptools', 'scipy', 'numpy', 'cython', 'pylab', 'PyQT4', 'matplotlib'],
      )
 
