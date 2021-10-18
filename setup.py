701549e598e5b510f7cd76b42b40986680032f16
Charlotte Mason <charlotte.nosam@gmail.com>
2021-10-18 11:45:08 +0200

    remove distribute requirement

--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ setup(name='PyQt-Fit',
                                  ]
                     },
       scripts=['bin/pyqt_fit1d.py'],
-      install_requires=['distribute >=0.6',
+      install_requires=[
+                        #'distribute >=0.6',
                         'numpy >=1.5.0',
                         'scipy >=0.10.0',
                         'matplotlib',
